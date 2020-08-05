781f201e95a5d174a8e0c3965b08c032200bc6a4
ksenia.ilina <44381451+kse1920@users.noreply.github.com>
2020-08-05 16:35:43 +0300

    Update TestCommonCode.kt

--- src/commonTest/kotlin/TestCommonCode.kt
+++ src/commonTest/kotlin/TestCommonCode.kt
@@ -3,17 +3,13 @@ package com.example.lib
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-@DisplayName("Common Code Test")
 class TestCommonCode {
 	@Test
-	@DisplayName("Test id")
 	fun testId() {
 		val x = 1
 		val idX = 3
 		assertEquals(x, idX)
 	}
-
-	@DisplayName("Second Test")
 	@Test
 	fun testExpectedFun() {
 		expectedFun()
